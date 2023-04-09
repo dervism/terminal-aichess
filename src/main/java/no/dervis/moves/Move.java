816095bf2cb7d21a50dd01ed610a65c088f22cfd
dervism <dervis4@gmail.com>
2023-04-09 19:48:50 +0200

    Add a record to decode and hold the parts of a move.

---
+++ src/main/java/no/dervis/moves/Move.java
@@ -0,0 +1,91 @@
+package no.dervis.moves;
+
+import no.dervis.Bitboard;
+import no.dervis.Chess;
+
+public record Move(
+        int piece,
+        int color,
+        int fromSquare,
+        int toSquare,
+        int moveType,
+        int promotionPiece) {
+
+    public static Move createMove(
+            int piece,
+            int color,
+            int fromSquare,
+            int toSquare
+    ) {
+        return new Move(
+                piece,
+                color,
+                fromSquare,
+                toSquare,
+                Chess.MoveType.NORMAL.ordinal(),
+                0);
+    }
+
+    public static Move createWhiteMove(
+            int piece,
+            int fromSquare,
+            int toSquare
+    ) {
+        return new Move(
+                piece,
+                Chess.white,
+                fromSquare,
+                toSquare,
+                Chess.MoveType.NORMAL.ordinal(),
+                0);
+    }
+
+    public static Move createMove(
+            int piece,
+            int color,
+            int fromSquare,
+            int toSquare,
+            int moveType,
+            int promotionPiece
+    ) {
+        return new Move(
+                piece,
+                color,
+                fromSquare,
+                toSquare,
+                moveType,
+                promotionPiece);
+    }
+
+    public static Move createMove(int move, Bitboard board){
+        int fromSquare = move >>> 14;
+        int toSquare = (move >>> 7) & 0x3F;
+        int moveType = (move >>> 4) & 0x7;
+        int promotionPiece = move & 0xF;
+
+        int piece = board.getPiece(fromSquare);
+        int color = piece / 6;
+        int pieceType = piece % 6;
+
+        return new Move(
+                pieceType,
+                color,
+                fromSquare,
+                toSquare,
+                moveType,
+                promotionPiece
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "Move{" +
+                "piece=" + piece +
+                ", color=" + color +
+                ", fromSquare=" + fromSquare +
+                ", toSquare=" + toSquare +
+                ", moveType=" + moveType +
+                ", promotionPiece=" + promotionPiece +
+                '}';
+    }
+}
