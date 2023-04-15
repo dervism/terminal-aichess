eb930c7807b28240df5550685ddd17e7ddfcef55
dervism <dervis4@gmail.com>
2023-04-16 00:34:47 +0200

    Update the castling tests

--- src/test/java/no/dervis/terminal_aichess/BitboardTest.java
+++ src/test/java/no/dervis/terminal_aichess/BitboardTest.java
@@ -274,32 +274,68 @@ class BitboardTest implements Board, Chess {
     @Test
     void castlingNotAllowed() {
         Bitboard board = new Bitboard();
-
         board.setPiece(king, white, e1.index());
-        board.setPiece(bishop, white, f1.index());
-        board.setPiece(bishop, black, f8.index());
         board.setPiece(king, black, e8.index());
-        board.setPiece(rook, white, h1.index());
-        board.setPiece(rook, black, h8.index());
+
+        // white queen side
         board.setPiece(rook, white, a1.index());
-        board.setPiece(rook, black, a8.index());
         board.setPiece(knight, white, b1.index());
+        board.setPiece(bishop, white, c1.index());
+        board.setPiece(queen, white, d1.index());
+
+        // white king side
+        board.setPiece(bishop, white, f1.index());
+        board.setPiece(knight, white, g1.index());
+        board.setPiece(rook, white, h1.index());
+
+        // black king side
+        board.setPiece(bishop, black, f8.index());
+        board.setPiece(knight, black, g8.index());
+        board.setPiece(rook, black, h8.index());
+
+        // black queen side
+        board.setPiece(rook, black, a8.index());
         board.setPiece(knight, black, b8.index());
+        board.setPiece(bishop, black, c8.index());
+        board.setPiece(queen, black, d8.index());
+
         System.out.println(boardToStr.apply(board, false));
 
+        // when all officers are in place, no castling moves should be generated
         Generator g = new Generator(board);
         List<T2<Integer, Move>> whiteMoves = g.generateMoves(white).stream()
                 .map(move -> new T2<>(move, Move.createMove(move, board)))
                 .filter(onlyCastlingMoves)
-                .peek(System.out::println)
                 .toList();
         assertEquals(0, whiteMoves.size());
 
         List<T2<Integer, Move>> blackMoves = g.generateMoves(black).stream()
                 .map(move -> new T2<>(move, Move.createMove(move, board)))
                 .filter(onlyCastlingMoves)
-                .peek(System.out::println)
                 .toList();
         assertEquals(0, blackMoves.size());
+
+        // remove the pieces on the queen side and verify castle queen side is now possible
+        board.removePiece(knight, white, b1.index());
+        board.removePiece(bishop, white, c1.index());
+        board.removePiece(queen, white, d1.index());
+
+        List<T2<Integer, Move>> queensideCastleMove = g.generateMoves(white).stream()
+                .map(move -> new T2<>(move, Move.createMove(move, board)))
+                .filter(onlyCastlingMoves)
+                .toList();
+        assertEquals(1, queensideCastleMove.size());
+        assertEquals(MoveType.CASTLE_QUEEN_SIDE.ordinal(), Move.createMove(queensideCastleMove.get(0).left(), board).moveType());
+
+        board.removePiece(knight, black, b8.index());
+        board.removePiece(bishop, black, c8.index());
+        board.removePiece(queen, black, d8.index());
+
+        List<T2<Integer, Move>> queensideCastleMoveBlack = g.generateMoves(black).stream()
+                .map(move -> new T2<>(move, Move.createMove(move, board)))
+                .filter(onlyCastlingMoves)
+                .toList();
+        assertEquals(1, queensideCastleMoveBlack.size());
+        assertEquals(MoveType.CASTLE_QUEEN_SIDE.ordinal(), Move.createMove(queensideCastleMoveBlack.get(0).left(), board).moveType());
     }
 }
